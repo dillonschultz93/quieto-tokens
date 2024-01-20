78f6531f261dfabab54f4d119c700a9fa95c6861
Dillon Schultz <dillonschultz93@gmail.com>
2024-01-19 17:56:33 -0800

    Create a light and dark mode token registration

--- src/build.js
+++ src/build.js
@@ -43,6 +43,7 @@ StyleDictionary.registerTransform({
   },
 });
 
+// Build core tokens
 const StyleDictionaryExtended = StyleDictionary.extend({
   source: ['src/tokens/core.json'],
   platforms: {
@@ -51,50 +52,48 @@ const StyleDictionaryExtended = StyleDictionary.extend({
       transforms: ['typography/css/px', 'typography/css/fontStack', 'typography/css/fontWeight', 'name/cti/kebab'],
       buildPath: 'dist/css/',
       files: [{
-        destination: '_variables.css',
+        destination: '_core-variables.css',
         format: 'css/variables',
       }],
     },
   },
 });
 
-// Register custom transforms
-// Append 'px' to fontSizes, lineHeights, letterSpacing, spacing, borderRadius, and borderWidth
-StyleDictionary.registerTransform({
-  type: 'value',
-  name: 'typography/css/px',
-  matcher: (prop) => prop.type === 'fontSizes' || prop.type === 'lineHeights' || prop.type === 'letterSpacing' || prop.type === 'spacing' || prop.type === 'borderRadius' || prop.type === 'borderWidth',
-  transformer: (prop) => `${prop.value}px`,
+// Build light token set
+const StyleDictionaryLightSet = StyleDictionary.extend({
+  source: ['src/tokens/core.json', 'src/tokens/light.json'],
+  platforms: {
+    css: {
+      transforms: ['name/cti/kebab'],
+      buildPath: 'dist/css/',
+      files: [{
+        filter: (token) => token.filePath.includes('light.json'),
+        destination: '_light-variables.css',
+        format: 'css/variables',
+      }],
+    },
+  },
 });
 
-// Convert font family name to a proper CSS font stack
-StyleDictionary.registerTransform({
-  type: 'value',
-  name: 'typography/css/fontStack',
-  matcher: (prop) => prop.type === 'fontFamilies',
-  transformer: () => '-apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif',
-});
-
-// Convert font weight to a proper CSS font weight
-StyleDictionary.registerTransform({
-  type: 'value',
-  name: 'typography/css/fontWeight',
-  matcher: (prop) => prop.type === 'fontWeights',
-  transformer: (prop) => {
-    switch (prop.value) {
-      case 'Light':
-        return '300';
-      case 'Regular':
-        return '400';
-      case 'Medium':
-        return '500';
-      case 'Bold':
-        return '700';
-      default:
-        return prop.value;
-    }
+// Build light token set
+const StyleDictionaryDarkSet = StyleDictionary.extend({
+  source: ['src/tokens/core.json', 'src/tokens/dark.json'],
+  platforms: {
+    css: {
+      transforms: ['name/cti/kebab'],
+      buildPath: 'dist/css/',
+      files: [{
+        filter: (token) => token.filePath.includes('dark.json'),
+        destination: '_dark-variables.css',
+        format: 'css/variables',
+      }],
+    },
   },
 });
 
 StyleDictionaryExtended.cleanAllPlatforms();
 StyleDictionaryExtended.buildAllPlatforms();
+StyleDictionaryLightSet.cleanAllPlatforms();
+StyleDictionaryLightSet.buildAllPlatforms();
+StyleDictionaryDarkSet.cleanAllPlatforms();
+StyleDictionaryDarkSet.buildAllPlatforms();
